9b9f296ae48cd18f2a09984ce04b6b35b8c33cfc
Remi BONNET <bonnet@fanaticalhelp.com>
2025-02-26 14:37:52 +0100

    Remove old XIVAPI and update nextJS

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,13 +1,7 @@
-import { GetCompanyMembers } from '@lib/parser';
 import CustomSWRConfig from '@lib/SWRConfig';
 
 export default async function PublicLayout({ children }) {
     const fallback = {}
-    try {
-        fallback[`${process.env.NEXT_PUBLIC_URL}/api/freecompany/9231253336202720275/members.json`] = await GetCompanyMembers()
-    } catch (e) {
-
-    }
 
     return <CustomSWRConfig fallback={fallback}>{children}</CustomSWRConfig>
 }
